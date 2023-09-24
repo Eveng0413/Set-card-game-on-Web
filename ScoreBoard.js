7796113dd39ee6d2546be2ab44a44feaf92094d9
Qiao Xiao <no.1fuze@gmail.com>
2023-09-24 17:01:09 -0400

    comments done

--- ScoreBoard.js
+++ ScoreBoard.js
@@ -1,6 +1,16 @@
- 
+ /** 
+ * Display score
+ * 
+ * @param scoreNum
+ *          the score number that will be displayed in scoreboard
+ * @returns 
+ * 
+ * @calls
+ */
  export function displayScore(scoreNum) {
+  //get element
   var scoreBox = document.getElementById('scoreNum');
+  //update content
   scoreBox.textContent = scoreNum;
 }
 
